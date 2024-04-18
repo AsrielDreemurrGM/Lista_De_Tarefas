d86c6c2050c58bb34992ced62a4bbe77837d53f5
AsrielDreemurrGM <eduardo.motta.980315@gmail.com>
2024-04-17 23:35:22 -0300

    Finalizado JQuery

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    //Adicionar Nova Tarefa
     $('form').on('submit', function(e) {
         e.preventDefault();
 
@@ -8,5 +9,10 @@ $(document).ready(function(){
         $(`<a>${novaTarefa}</a>`).appendTo(novaLinha);
         $(novaLinha).appendTo('ul');
         $('#tarefa').val('');
+    })
+    //Fim da Função Para Adicionar Nova Tarefa
+    //inicio da Função Para Marcar Tarefa Como Concluida
+    $('ul').on('click', 'li', function() { //Encontra o 'ul', e quando um 'li' é clicado segue com o código
+        $(this).find('a').toggleClass('tarefa-concluida'); //".find" procura o 'a' especifico do 'li' clicado
     })
 })
